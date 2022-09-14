0a8bf7e57e8ef205e6645e7a7b9c21e671ce42a0
Nyzo <nyzo@nyzo.co>
2022-09-13 22:07:38 -0400

    Updated scoping in ByteBuffer JavaScript class.

--- js/byteBuffer.js
+++ js/byteBuffer.js
@@ -11,7 +11,7 @@ class ByteBuffer {
     }
 
     putBytes(bytes) {
-        for (var i = 0; i < bytes.length; i++) {
+        for (let i = 0; i < bytes.length; i++) {
             this.array[this.index++] = bytes[i];
         }
     }
@@ -22,7 +22,7 @@ class ByteBuffer {
 
     putIntegerValue(value, length) {
         value = Math.floor(value);
-        for (var i = 0; i < length; i++) {
+        for (let i = 0; i < length; i++) {
             this.array[this.index + length - 1 - i] = value % 256;
             value = Math.floor(value / 256);
         }
@@ -42,8 +42,8 @@ class ByteBuffer {
     }
 
     toArray() {
-        var result = new Uint8Array(this.index);
-        for (var i = 0; i < this.index; i++) {
+        let result = new Uint8Array(this.index);
+        for (let i = 0; i < this.index; i++) {
             result[i] = this.array[i];
         }
 
